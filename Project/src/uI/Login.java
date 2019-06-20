a62cab301f3ebe73e0f24fc2bb4298b886bcba8c
Shakiba <star.77279@yahoo.com>
2019-06-21 01:29:35 +0430

    Sign up button added. frame moved to the center of the windows

--- Project/src/uI/Login.java
+++ Project/src/uI/Login.java
@@ -16,23 +16,24 @@ import users.User;
 public class Login {
 	
 	public Login(){
-		JFrame frame = new JFrame("Login");
-		ImageIcon image = new ImageIcon("D:\\Java Codes\\Project\\Spotify.jpg");
-		frame.setIconImage(image.getImage());
-	    JPasswordField password = new JPasswordField();
+	    
+		JPasswordField password = new JPasswordField();
 	    password.setBounds(100, 100, 140, 30);
 	    password.setToolTipText("Enter your Password");
 	    JLabel passLable = new JLabel("Password:");
 	    passLable.setBounds(10,100, 80,30);
+	    
+	    
 	    JTextField email = new JTextField();
 	    email.setBounds(100, 50, 140, 30);
 	    email.setToolTipText("Enter your Email");
 	    JLabel emailLabel = new JLabel("Email:");
 	    emailLabel.setBounds(20, 50, 80, 30);
-	    JButton button = new JButton("Login");
-	    button.setBounds(100, 200, 100, 30);
-	    button.addActionListener(new ActionListener() {
-			
+	    
+	    
+	    JButton loginButton = new JButton("Login");
+	    loginButton.setBounds(100, 200, 100, 30);
+	    loginButton.addActionListener(new ActionListener() {	
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				User newUser = new User(email.getText(), password.getText());
@@ -41,15 +42,35 @@ public class Login {
 			}
 		});
 	    
+	    
+	    JButton signUpButton = new JButton("Sign Up");
+	    signUpButton.setBounds(100, 250, 100, 30);
+	    signUpButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new SignUp();
+			}
+		});
+	    
+	    
+	    
+	    JFrame frame = new JFrame("Login");
+		ImageIcon image = new ImageIcon("D:\\Java Codes\\Project\\Spotify.jpg");
+		frame.setIconImage(image.getImage());
 	    frame.add(email);
 	    frame.add(emailLabel);
 	    frame.add(password);
 	    frame.add(passLable);
-	    frame.add(button);
-	    frame.setSize(300,300);
+	    frame.add(loginButton);
+	    frame.add(signUpButton);
+	    frame.setBounds(600, 200, 300, 400);
 	    frame.setLayout(null);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	}
+	
+	public static void main(String[] args) {
+		new Login();
 	}
 }
