c06f5f9a6eac37fb68614caf7bb10342feebf542
Roonak <rezaie.roonak2000@gmail.com>
2019-06-21 01:51:09 +0430

    WestPanel class eddited

--- src/WestPanel.java
+++ src/WestPanel.java
@@ -8,21 +8,20 @@ public class WestPanel extends JScrollPane {
         mainPanel.setSize(100 , 800);
         GridBagLayout ly=new GridBagLayout();
         GridBagConstraints gbc=new GridBagConstraints();
-        gbc.fill = GridBagConstraints.VERTICAL;
         mainPanel.setLayout(ly);
-
-        setViewportView(mainPanel);
-        setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        gbc.fill = GridBagConstraints.VERTICAL;
 
 
         gbc.gridx = 0;
         gbc.gridy = 0;
         JButton Home = new JButton("Home");
+        Home.setSize(new Dimension(50 , 100));
         mainPanel.add(Home , gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
         JButton addMusic = new JButton("addMusic");
+        addMusic.setSize(new Dimension(50 , 100));
         mainPanel.add(addMusic , gbc);
 
         JButton jButton=new JButton("Library");
@@ -31,36 +30,44 @@ public class WestPanel extends JScrollPane {
 
         gbc.gridx=0;
         gbc.gridy=2;
+        jButton.setSize(new Dimension(50 , 100));
         mainPanel.add(jButton,gbc);
 
         JButton Albums=new JButton("Alubms");
         gbc.gridx=0;
         gbc.gridy=3;
+        Albums.setSize(new Dimension(50 , 100));
         mainPanel.add(Albums,gbc);
 
 
         JButton Songs=new JButton("Songs");
         gbc.gridx=0;
         gbc.gridy=4;
+        Songs.setSize(new Dimension(50 , 100));
         mainPanel.add(Songs,gbc);
 
 
         JButton PlayList=new JButton("PlayList");
         gbc.gridx=0;
         gbc.gridy=5;
+        PlayList.setSize(new Dimension(50 , 100));
         mainPanel.add(PlayList,gbc);
 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.ipady = 0;       //reset to default
-        gbc.weighty = 1.0;   //request any extra vertical space
+        gbc.weighty = 2;   //request any extra vertical space
         gbc.anchor = GridBagConstraints.PAGE_END; //bottom of space
         gbc.insets = new Insets(10,0,0,0);  //top padding
         gbc.gridx = 0;       //aligned with button 2
         gbc.gridwidth = 2;   //2 columns wide
-        gbc.gridy = 3;
-        JButton newPlayList = new JButton("newPlayList");
+        gbc.gridy = 6;
+        JButton newPlayList = new JButton("newPlayList" );
+        newPlayList.setSize(new Dimension(50 , 100));
         mainPanel.add(newPlayList , gbc);
 
+
+        setViewportView(mainPanel);
+        setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
 
 
