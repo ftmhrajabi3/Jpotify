24fc7b77098d2356e572c694b3ee54987650f22e
Roonak <rezaie.roonak2000@gmail.com>
2019-06-21 11:31:43 +0430

    editting search textfield

--- src/North.java
+++ src/North.java
@@ -7,26 +7,13 @@ public class North extends JPanel {
 
     public North() {
         JLabel label = new JLabel("Search : ");
-       // JTextField jTextField=new JTextField(1);
-        GridLayout ly = new GridLayout();
         label.setVisible(true);
-//        setLayout(new FlowLayout(1));
-        setLayout(ly);
 
+        FlowLayout fly = new FlowLayout(FlowLayout.LEFT);
+        setLayout(fly);
+        search.setColumns(20);
 
-        GridBagConstraints gbc=new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx=0;
-        gbc.gridy=0;
-        gbc.gridwidth=1;
-        gbc.gridheight=1;
-        label.setVisible(true);
-        this.add(label,gbc);
-
-        gbc.gridx=1;
-        gbc.gridheight=5;
-        gbc.weightx=1;
-        this.add(search , gbc );
-
+        this.add(label);
+        this.add(search );
     }
 }
